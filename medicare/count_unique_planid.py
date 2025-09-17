812ff4f640f851e8ab1551935f371a82cdea68e3
mckinlde <douglas.e.mckinley@gmail.com>
2025-09-17 11:00:54 -0700

    add the printing back

--- medicare/count_unique_planid.py
+++ medicare/count_unique_planid.py
@@ -16,12 +16,21 @@ total_unique_count = df["plan_name"].nunique()
 print("Total unique plan_name count:", total_unique_count)
 
 
-# # Sort by count descending
-# sorted_items = sorted(unique_counts.items(), key=lambda item: item[1], reverse=True)
+# Sort by count descending
+sorted_items = sorted(unique_counts.items(), key=lambda item: item[1], reverse=True)
 
-# # Print as list of tuples
-# pprint.pprint(sorted_items)
+# Print as list of tuples
+pprint.pprint(sorted_items)
 
-# # Have to override the default dict sorting to keep it in order for human reading
-# sorted_dict = OrderedDict(sorted_items)
-# pprint.pprint(sorted_dict)
+# Have to override the default dict sorting to keep it in order for human reading
+sorted_dict = OrderedDict(sorted_items)
+pprint.pprint(sorted_dict)
+
+# Sites for specific companies
+# UnitedHealthcare,  www.uhc.com
+# Aetna, www.aetna.com
+# Humana, not reliable
+# HCSC, www.cigna.com
+# Devoted Health, www.devoted.com
+# Wellcare, not reliable
+# Kaiser Permanente, healthy.kaiserpermanente.org
